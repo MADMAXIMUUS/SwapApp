dcd272e3a28d082d20acc8e224ae7470efc0aa52
MADMAXIMUUS <maxim00nechaev@gmail.com>
2022-04-03 14:44:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/swap/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.swap
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.swap.ui.theme.SwapTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DefaultPreview()
+        }
+    }
+}
+
+
+@Preview(showBackground = true, backgroundColor = 0xffff, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    SwapTheme {
+        Column() {
+            
+        }
+    }
+}
---
+++ app/src/main/java/com/example/swap/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.swap.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Yellow = Color(0xFFEDB408)
+val Light_brown = Color(0xFFCC7112)
+val Depp_dark_blue = Color(0xFF002B52)
+val Night_blue = Color(0xFF16243C)
