80953c4e6873175c651e61925926daf9a704655e
MADMAXIMUUS <maxim00nechaev@gmail.com>
2022-06-27 18:19:45 +0300

    Fixes camera

--- app/src/main/java/com/example/swap/presentation/camerascreen/CameraScreen.kt
+++ app/src/main/java/com/example/swap/presentation/camerascreen/CameraScreen.kt
@@ -2,7 +2,10 @@ package com.example.swap.presentation.camerascreen
 
 import android.content.Context
 import android.net.Uri
-import androidx.camera.core.*
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.background
@@ -12,27 +15,31 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavController
-import com.example.swap.R
 import com.example.swap.ui.theme.*
 import com.example.swap.utilities.getDirectory
-import com.example.swap.utilities.showToast
+import com.skydoves.landscapist.glide.GlideImage
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 @Composable
 fun CameraScreen(navController: NavController) {
@@ -44,7 +51,52 @@ fun CameraScreen(navController: NavController) {
         context = context,
         lifecycleOwner = lifecycleOwner,
         outputDirectory = getDirectory(context),
-        onMediaCaptured = { url -> navController.popBackStack()}
+        onMediaCaptured = { uris ->
+            navController.popBackStack()
+        },
+        onError = {
+
+        }
+    )
+}
+
+private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
+    suspendCoroutine { continuation ->
+        ProcessCameraProvider.getInstance(this).also { cameraProvider ->
+            cameraProvider.addListener({
+                continuation.resume(cameraProvider.get())
+            }, ContextCompat.getMainExecutor(this))
+        }
+    }
+
+private fun takePhoto(
+    filenameFormat: String,
+    imageCapture: ImageCapture,
+    outputDirectory: File,
+    executor: Executor,
+    onMediaCaptured: (Uri) -> Unit,
+    onError: (String) -> Unit
+) {
+    val photoFile = File(
+        outputDirectory,
+        SimpleDateFormat(filenameFormat, Locale.US).format(System.currentTimeMillis()) + ".jpg"
+    )
+
+    val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+    imageCapture.takePicture(
+        outputOptions,
+        executor,
+        object : ImageCapture.OnImageSavedCallback {
+            override fun onError(exception: ImageCaptureException) {
+                onError(exception.localizedMessage ?: "Что-то пошло не так")
+            }
+
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(photoFile)
+                onMediaCaptured(savedUri)
+            }
+        }
     )
 }
 
@@ -54,69 +106,77 @@ fun SimpleCameraPreview(
     context: Context,
     lifecycleOwner: LifecycleOwner,
     outputDirectory: File,
-    onMediaCaptured: (Uri?) -> Unit
+    onMediaCaptured: (MutableList<Uri>) -> Unit,
+    onError: (String) -> Unit
 ) {
-    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
-    var imageCapture: ImageCapture? by remember { mutableStateOf(null) }
-    var preview by remember { mutableStateOf<Preview?>(null) }
-    val camera: Camera? = null
-    var lensFacing by remember { mutableStateOf(CameraSelector.LENS_FACING_BACK) }
-    var flashEnabled by remember { mutableStateOf(false) }
-    var flashRes by remember { mutableStateOf(R.drawable.ic_flash_off) }
-    val executor = ContextCompat.getMainExecutor(context)
-    var cameraSelector: CameraSelector
-    val cameraProvider = cameraProviderFuture.get()
+    val imgList = remember { mutableStateListOf<Uri>(Uri.EMPTY) }
+    val showBar = remember {
+        mutableStateOf(false)
+    }
+    val imageCapture: ImageCapture = remember { ImageCapture.Builder().build() }
+    val preview = Preview.Builder().build()
+    val previewView = remember { PreviewView(context) }
+    //val camera: Camera? = null
+    val lensFacing = remember { mutableStateOf(CameraSelector.LENS_FACING_BACK) }
+    /*val flashEnabled by remember { mutableStateOf(false) }
+    val flashRes by remember { mutableStateOf(R.drawable.ic_flash_off) }*/
+    val executor = Executors.newSingleThreadExecutor()
+    val cameraSelector = CameraSelector
+        .Builder()
+        .requireLensFacing(lensFacing.value)
+        .build()
+    //val cameraProvider = cameraProviderFuture.get()
 
-    Box(modifier = modifier) {
-        AndroidView(
-            modifier = Modifier
-                .fillMaxSize(),
-            factory = { ctx ->
-                val previewView = PreviewView(ctx)
-                cameraProviderFuture.addListener({
-                    val imageAnalysis = ImageAnalysis.Builder()
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build()
-                        .apply {
-                            setAnalyzer(executor, FaceAnalyzer())
-                        }
-                    imageCapture = ImageCapture.Builder()
-                        .setTargetRotation(previewView.display.rotation)
-                        .build()
-
-                    cameraSelector = CameraSelector.Builder()
-                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                        .build()
-
-                    cameraProvider.unbindAll()
-                    cameraProvider.bindToLifecycle(
-                        lifecycleOwner,
-                        cameraSelector,
-                        imageCapture,
-                        preview
-                    )
-                }, executor)
-                preview = Preview.Builder().build().also {
-                    it.setSurfaceProvider(previewView.surfaceProvider)
-                }
-                previewView
-            }
+    LaunchedEffect(lensFacing) {
+        val cameraProvider = context.getCameraProvider()
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner,
+            cameraSelector,
+            preview,
+            imageCapture
         )
 
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+    }
+
+    Column(modifier = modifier) {
+        AndroidView(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp),
+            factory = { previewView }
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .background(if (isSystemInDarkTheme()) Dark_bottom_top_bar else White)
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                text = "Наведите камеру на объект и сфотографируйте его",
+                style = MaterialTheme.typography.h2,
+                textAlign = TextAlign.Center,
+                color = if (isSystemInDarkTheme()) Yellow else Deep_dark_blue
+            )
+        }
         Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(200.dp)
+                .weight(0.3f)
                 .background(
                     color = if (isSystemInDarkTheme()) Dark_bottom_top_bar else White
                 )
-                .align(Alignment.BottomCenter)
         ) {
-            Spacer(modifier = Modifier.width(0.dp))
+            /*Spacer(modifier = Modifier.width(0.dp))
             IconButton(onClick = {
-                camera?.let {
+                *//*camera?.let {
                     if (it.cameraInfo.hasFlashUnit()) {
                         flashEnabled = !flashEnabled
                         flashRes = if (flashEnabled)
@@ -125,10 +185,10 @@ fun SimpleCameraPreview(
                             R.drawable.ic_flash_on
                         it.cameraControl.enableTorch(flashEnabled)
                     }
-                }
+                }*//*
             }) {
                 Icon(
-                    painter = painterResource(id = flashRes),
+                    painter = painterResource(id = R.drawable.ic_flash_on),
                     contentDescription = "Flashlight button",
                     modifier = Modifier.size(40.dp),
                     tint = if (isSystemInDarkTheme())
@@ -136,34 +196,22 @@ fun SimpleCameraPreview(
                     else
                         Night_blue
                 )
-            }
+            }*/
             Button(
                 onClick = {
-                    val imgCapture = imageCapture ?: return@Button
-                    val photoFile = File(
-                        outputDirectory,
-                        SimpleDateFormat("yyyyMMDD-HHmmss", Locale.US)
-                            .format(System.currentTimeMillis()) + ".jpg"
-                    )
-                    val outputFileOptions = ImageCapture
-                        .OutputFileOptions
-                        .Builder(photoFile)
-                        .build()
-                    imgCapture.takePicture(
-                        outputFileOptions,
-                        executor,
-                        object : ImageCapture.OnImageSavedCallback {
-                            override fun onImageSaved(
-                                outputFileResults: ImageCapture.OutputFileResults
-                            ) {
-                                onMediaCaptured(Uri.fromFile(photoFile))
-                            }
-
-                            override fun onError(exception: ImageCaptureException) {
-                                showToast("Something went wrong", context)
-                            }
-
-                        }
+                    takePhoto(
+                        filenameFormat = "yyyy-MM-dd-HH-mm-ss-SSS",
+                        imageCapture = imageCapture,
+                        outputDirectory = outputDirectory,
+                        executor = executor,
+                        onMediaCaptured = { uri ->
+                            if (imgList.size == 1)
+                                imgList[0] = uri
+                            else
+                                imgList.add(uri)
+                            showBar.value = true
+                        },
+                        onError = onError
                     )
                 },
                 modifier = Modifier
@@ -178,7 +226,7 @@ fun SimpleCameraPreview(
                 content = {}
             )
 
-            IconButton(
+            /*IconButton(
                 onClick = {
                     lensFacing =
                         if (lensFacing == CameraSelector.LENS_FACING_BACK) CameraSelector.LENS_FACING_FRONT
@@ -206,7 +254,71 @@ fun SimpleCameraPreview(
                         Night_blue
                 )
             }
-            Spacer(modifier = Modifier.width(0.dp))
+            Spacer(modifier = Modifier.width(0.dp))*/
+        }
+        if (showBar.value) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .background(
+                        if (isSystemInDarkTheme())
+                            Dark_bottom_top_bar
+                        else
+                            White
+                    )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                        .background(
+                            if (isSystemInDarkTheme())
+                                Dark_Content_Background
+                            else
+                                Color.LightGray
+                        ),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    GlideImage(
+                        modifier = Modifier
+                            .size(80.dp, 70.dp)
+                            .padding(8.dp)
+                            .clip(RoundedCornerShape(10.dp)),
+                        imageModel = imgList[0],
+                        contentDescription = ""
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        modifier = Modifier
+                            .weight(0.5f),
+                        text = "1 фото",
+                        style = MaterialTheme.typography.h1,
+                        color = if (isSystemInDarkTheme()) Yellow else Deep_dark_blue
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(
+                        onClick = {
+
+                        },
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(10.dp)),
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Light_brown
+                        ),
+                        content = {
+                            Text(
+                                text = "Готово",
+                                style = MaterialTheme.typography.button,
+                                color = if (isSystemInDarkTheme()) Dark_Background else White
+                            )
+                        }
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                }
+            }
         }
     }
 }
